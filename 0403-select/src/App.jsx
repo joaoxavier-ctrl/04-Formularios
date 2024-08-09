73a2c45fdf843a57533295cb90bfcfdf07e0dabd
JoaoXavierWin <joao.xavier21@gmail.com>
2024-08-09 14:28:16 -0300

    select

--- 0403-select/src/App.jsx
+++ 0403-select/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App reacts</div>;
-}
+  const [select, setSelect] = React.useState('');
+
+  return (
+    <form>
+      <select value={select} onChange={({target}) => setSelect(target.value)} id="produtos">
+        <option disabled value="">Selecione</option>
+        <option value="notebook">Notebook</option>
+        <option value="smartphone">Smartphone</option>
+        <option value="tablet">Tablet</option>
+        </select>
+        {select}
+    </form>
+  )
+};
 
 export default App
