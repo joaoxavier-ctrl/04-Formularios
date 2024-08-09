7fb4c8c5bb737e0fc3bd762f6067591bdc3987ae
JoaoXavierWin <joao.xavier21@gmail.com>
2024-08-09 15:40:55 -0300

    checkbox-1

--- 0405-checkbox/src/App.jsx
+++ 0405-checkbox/src/App.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App reacts</div>;
+  const [termos, setTermos] = React.useState(false);
+  const [cores, setCores] = React.useState([]);
+
+  function handleChange({target}){
+    setCores([...cores, target.value])
+  }
+
+  return (
+    <form>
+      <label>
+        <input 
+          type="checkbox" 
+          value="Termos" 
+          checked={termos} 
+          onChange={({target}) => setTermos(target.checked)}
+          />
+        Aceito os termos.
+      </label>*/
+      <label>
+        <input 
+          type="checkbox" 
+          value="Termos" 
+          //checked={termos} 
+          onChange={handleChange}
+          />
+        Azul
+      </label>
+      <label>
+        <input 
+          type="checkbox" 
+          value="Termos" 
+          //checked={termos} 
+          onChange={handleChange}
+          />
+        Vermelho
+      </label>
+    </form>
+  )
 }
 
 export default App
