ac2353ccaa19b7fe0ec6b1a6a1664744ea52f9bf
JoaoXavierWin <joao.xavier21@gmail.com>
2024-08-10 23:55:02 -0300

    componentes-radio-finalizado-e-fix-radio-style

--- 0408-componentes-radio/src/App.jsx
+++ 0408-componentes-radio/src/App.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import Input from './Form/Input'
+import Select from './Form/Select';
+import Radio from './Form/Radio';
 
 const App = () => {
-  return <div>App reacts</div>;
-}
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
+
+  return (
+    <form>
+      <h2>Cores</h2>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor}/>
+      <h2>Frutas</h2>
+      <Radio options={['Mamão', 'MURANGO']} value={cor} setValue={setCor}/>
+
+      <Select 
+        options={['Smartphone', 'Tablet']} 
+        value={produto} 
+        setValue={setProduto}
+      />
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} required/>
+      <Input id="email" label="Email" value={email} setValue={setEmail}/>
+    </form>
+  );
+};
 
 export default App
