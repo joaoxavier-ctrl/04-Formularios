37b38d008454da69562d4a783ffe821bcf151891
JoaoXavierWin <joao.xavier21@gmail.com>
2024-08-10 22:52:03 -0300

    aula-componente-input-finalizada

--- 0406-componente-input/src/App.jsx
+++ 0406-componente-input/src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Input from './Form/Input'
 
 const App = () => {
-  return <div>App reacts</div>;
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  return (
+    <form>
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} required/>
+      <Input id="email" label="Email" value={email} setValue={setEmail}/>
+    </form>
+  );
 }
 
 export default App
